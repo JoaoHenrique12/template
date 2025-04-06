f62bf100ed50ff1e5ab407bd63192ed04096ad05
Joao Henrique <joao.henrique1299@hotmail.com>
2025-04-06 18:47:17 -0300

    fix(lazy): atlantis for sonokai

--- lua/plugins/user.lua
+++ lua/plugins/user.lua
@@ -75,7 +75,7 @@ return {
 
   {
     "sainnhe/sonokai",
-    init = function() -- init function runs before the plugin is loaded
+    config = function() -- init function runs before the plugin is loaded
       vim.g.sonokai_style = "atlantis"
     end,
   },
